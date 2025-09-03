a5f08079a08e53329e02e8eb1f2f2fa71aae9a9a
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2025-09-03 23:59:14 +0100

    feat: escrow initialization

--- programs/sol-escrow-smc/src/lib.rs
+++ programs/sol-escrow-smc/src/lib.rs
@@ -6,11 +6,14 @@ declare_id!("2jDJbiWwoGCi2k11h172zBiD657y1uByM8g92NRNLa4X");
 pub mod sol_escrow_smc {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    /// Initialize a new escrow
+    pub fn initialize_escrow(
+        ctx: Context<InitializeEscrow>,
+        escrow_id: u64,
+        token_a_amount: u64,
+        token_b_amount: u64,
+        timeout_duration: i64,
+    ) -> Result<()> {
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
